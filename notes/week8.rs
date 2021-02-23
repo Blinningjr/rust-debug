8b62e965de00178633ec2c6d046eccbaf192cfb8
Niklas <nicke.l@telia.com>
2021-02-23 21:46:11 +0100

    Added note on what I did today and yesterday

---
+++ notes/week8.rs
@@ -0,0 +1,18 @@
+# Monday 2021/02/22
+* Researching how to get more debug information for optimized code in the DWARF file.
+
+
+# Tuesday
+* Researching how to get more debug information for optimized code in the DWARF file.
+
+
+# Wednesday
+
+# Thursday
+
+# Friday
+
+# Saturday
+
+# Sunday
+
